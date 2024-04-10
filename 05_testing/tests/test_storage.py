9f24ec820f58bc6b037395995d972a588b23cb93
Kilian Kluge <dev@kluge.ai>
2024-04-10 13:53:47 +0200

    Weitere Storage-Tests

--- 05_testing/tests/test_storage.py
+++ 05_testing/tests/test_storage.py
@@ -10,12 +10,15 @@ from my_cloud.storage import StorageClient
 from my_cloud.exceptions import ResourceAlreadyExistsException
 
 
-def test_that_bucket_can_be_created(tmp_path):
+@pytest.mark.parametrize("foo", [1, 2, 3])
+def test_that_bucket_can_be_created(tmp_path, foo):
     # GIVEN
     c = StorageClient(tmp_path)
 
     # THEN
     c.create_bucket("my-test-bucket")
+
+    assert "my-test-bucket" in c.list_buckets()
 
 
 def test_that_bucket_names_must_be_unique(tmp_path):
@@ -23,7 +26,27 @@ def test_that_bucket_names_must_be_unique(tmp_path):
     c = StorageClient(tmp_path)
     c.create_bucket("my-test-bucket")
 
-    # THEN
+    # WHEN/THEN
     with pytest.raises(ResourceAlreadyExistsException):
         c.create_bucket("my-test-bucket")
+
+
+def test_that_bucket_creation_creates_folder(tmp_path):
+    # GIVEN
+    c = StorageClient(tmp_path)
+
+    # WHEN
+    c.create_bucket("my-test-bucket")
+
+    # THEN
+    assert (tmp_path / "my-test-bucket").exists()
+    assert (tmp_path / "my-test-bucket").is_dir()
+
+
+def test_that_storage_client_discovers_existing_files(tmp_path):
+    ...
+
+
+def test_that_storage_client_deletes_data_from_disk(tmp_path):
+    ...
 
