3bdbbce3d3f1b1f8dcf456c1acc06f541c1c7714
Kilian Kluge <dev@kluge.ai>
2024-04-10 13:21:27 +0200

    Storage-Tests

--- 05_testing/tests/test_storage.py
+++ 05_testing/tests/test_storage.py
@@ -5,3 +5,25 @@ Aufgabe 2: Schreibe Tests, die die Ablage im Dateisystem überprüfen.
 
 Aufgabe 3: Bringe die Testabdeckung auf 100%, indem du Tests für die Ausnahmebehandlung schreibst.
 """
+import pytest
+from my_cloud.storage import StorageClient
+from my_cloud.exceptions import ResourceAlreadyExistsException
+
+
+def test_that_bucket_can_be_created(tmp_path):
+    # GIVEN
+    c = StorageClient(tmp_path)
+
+    # THEN
+    c.create_bucket("my-test-bucket")
+
+
+def test_that_bucket_names_must_be_unique(tmp_path):
+    # GIVEN
+    c = StorageClient(tmp_path)
+    c.create_bucket("my-test-bucket")
+
+    # THEN
+    with pytest.raises(ResourceAlreadyExistsException):
+        c.create_bucket("my-test-bucket")
+
